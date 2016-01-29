06df66b9842d4754127e72cae010e02868baf344
Martin Chochod <martin.chochod@telecom-paristech.fr>
2016-01-29 16:07:09 +0100

    Code constructeur Light

--- Light.h
+++ Light.h
@@ -1,5 +1,17 @@
 #ifndef LIGHT_H
 #define LIGHT_H
+#include "Vec3.h"
 
+
+class Light{
+ public:
+  Light();
+  virtual ~Light();
+ private:
+  Vec3f pos;
+  float intensity;
+  Vec3f dir;
+  float angle;
+};
 
 #endif

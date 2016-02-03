105314ff929e0dac846a15422cb635da48c51025
Martin Chochod <martin.chochod@telecom-paristech.fr>
2016-02-03 23:21:33 +0100

    Avancée dans la fonction createLightTree

--- Light.cpp
+++ Light.cpp
@@ -4,33 +4,35 @@
 using namespace std;
 
 Light::Light(Vec3f _pos, float _intensity, Vec3f  _coneDir, Vec3f _coneAngle){
-  this.pos = _pos;
-  this.intensity = _intensity;
-  this.dir = _coneDir;
-  this.angle = _coneAngle;
+  this->pos = _pos;
+  this->intensity = _intensity;
+  this->dir = _coneDir;
+  this->angle = _coneAngle;
 }
 
 void Light::createLightTree(const vector<Light> & lightTable){
+  // (distance, closestNeighbour1Index, closestNeighbour2Index)
+  vector<tuple<float, float, float>> distTable;
+  int index = 0;
+  for(vector<Light>::const_iterator it = lightTable.begin(); it != lightTable.end()-1; it ++, index ++){
 
-  tuple<float, float, float> distTable;
-
-  for(int index=0, vector<Light>::iterator it = lightTable.begin(); it != lightTable.end()-1; it ++, index ++){
-
-    float min = -1.0f; //Supposed to be INFINITY WIP !!!
+    float min = INFINITY; //Supposed to be INFINITY WIP !!!
     float lightIndex = index;
     float distance = 0.0f;
-    for(index2 = index, vector<Light>::iterator jt = it+1; jt != lightTable.end(); jt ++, index2++){
+    int index2 = index;
+    for(vector<Light>::const_iterator jt = it+1; jt != lightTable.end(); jt ++, index2++){
       //calcule distance de it.pos à jt.pos
       distance = dist(it->pos, jt->pos);
       //if (distance < min) min = distance & lightIndex = actualIndex
       if(distance < min)
 	{
 	  min = distance;
-	  lightIndex = lightIndex;
+	  lightIndex = index2;
 	}
       }
-    }
-    //store min and lightIndex with itIndex in distTable for couple closest neighbors
+//store min and lightIndex with itIndex in distTable for couple closest neighbors
+    distTable.push_back(make_tuple(min,index,lightIndex));
+    }    
   }
   //Here the distTable is complete
 
--- Light.h
+++ Light.h
@@ -1,18 +1,20 @@
 #ifndef LIGHT_H
 #define LIGHT_H
 #include "Vec3.h"
+#include <vector>
 
+using namespace std;
 
 class Light{
- public:
-  Light();
-  virtual ~Light();
-  void createLightTree(const vector<Light> & lightTable);
- 
  private:
   Vec3f pos;
   float intensity;
   Vec3f dir;
-  float angle;
+  Vec3f angle;
+
+ public:
+  Light(Vec3f,float,Vec3f,Vec3f);
+  virtual ~Light();
+  void createLightTree(const  vector<Light> & lightTable);
 };
 #endif

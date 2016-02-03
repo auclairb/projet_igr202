aa278ca07597106e16fa577efddfd5cc38b7f30b
Martin Chochod <martin.chochod@telecom-paristech.fr>
2016-02-03 21:45:27 +0100

    WIP

--- Light.cpp
+++ Light.cpp
@@ -12,17 +12,23 @@ Light::Light(Vec3f _pos, float _intensity, Vec3f  _coneDir, Vec3f _coneAngle){
 
 void Light::createLightTree(const vector<Light> & lightTable){
 
-  {(float, float, float)...}  distTable;
+  tuple<float, float, float> distTable;
 
-  for(vector<Light>::iterator it = lightTable.begin(); it != lightTable.end()-1; it ++){
+  for(int index=0, vector<Light>::iterator it = lightTable.begin(); it != lightTable.end()-1; it ++, index ++){
 
-    float min = infinity;
-    float lightIndex = lui_meme;
-
-    for(vector<Light>::iterator jt = it+1; jt != lightTable.end(); jt ++){
+    float min = -1.0f; //Supposed to be INFINITY WIP !!!
+    float lightIndex = index;
+    float distance = 0.0f;
+    for(index2 = index, vector<Light>::iterator jt = it+1; jt != lightTable.end(); jt ++, index2++){
       //calcule distance de it.pos à jt.pos
-
+      distance = dist(it->pos, jt->pos);
       //if (distance < min) min = distance & lightIndex = actualIndex
+      if(distance < min)
+	{
+	  min = distance;
+	  lightIndex = lightIndex;
+	}
+      }
     }
     //store min and lightIndex with itIndex in distTable for couple closest neighbors
   }

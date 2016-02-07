da525d696f6d6b1eb23c3adc2786340e0f00620a
AUCLAIR <benjamin.auclair@telecom-paristech.fr>
2016-02-07 02:46:03 +0100

    j'ai fini le lightcut ! J'ai une erreur de compilation mais je sais pas trop d'où elle vient ! je verrais demain

--- Lightcut.cpp
+++ Lightcut.cpp
@@ -42,7 +42,7 @@ void Lightcut::allIntersects(Mesh& mesh, const vector<Light> & lightTable, int *
   }
 }
 
-
+/*
 void Lightcut::buildLightcut(ltuplist & clusterTable, Mesh& mesh, const vector<Light> & lightTable, float error){
   this->allIntersects(mesh, lightTable, result);
   ltuplist clusterTableCopy = clusterTable;
@@ -69,38 +69,75 @@ void Lightcut::buildLightcut(ltuplist & clusterTable, Mesh& mesh, const vector<L
     const Vertex & v = mesh.V[k];
     Light light = cut.back();
     Vec3f wi = (light.getPos()-v.p);
-    int geometry = 1 / (wi.length())*(wi.length());
-    int intensity = light.getIntensity();
+    float geometry = 1 / (wi.length())*(wi.length());
+    float intensity = light.getIntensity();
+    float visibility = (result[k])[j];
     if(radiance[k]!=0){
-      errorTest+=(radiance[k]-geometry*intensity)/radiance[k];
+      errorTest+=(radiance[k]-geometry*intensity*visibiliy)/radiance[k];
     }
   }
   
-  while(errorTest>error){
-    errorTest=0;
+  while(errorTest>error && cut.size()!= lightTable.size()){
     //float errorBound = INFINITY;
     //float errorRec;
+    Light root;
     Light son1;
     Light son2;
     
     for(vector<Light>::iterator it = cut.begin();it!=cut.end();it++){
       //errorRec=0;
-      /*for(ltuplist::iterator jt = clusterTable.begin(); jt!=clusterTable.end();jt++){
+      for(ltuplist::iterator jt = clusterTable.begin(); jt!=clusterTable.end();jt++){
 	if((get<0>(jt)).isEqual(it)){
 
 	  for(unsigned int k = 0; k<mesh.V.size();k++){
 	    const Vertex & v = mesh.V[k];
-	    Light light = cut.back();
-	    Vec3f wi = (light.getPos()-v.p);
-	    int geometry = 1 / (wi.length())*(wi.length());
-	    int intensity = light.getIntensity();
-	    if(radiance[k]!=0){
-	      errorTest+=(radiance[k]-geometry*intensity)/radiance[k];
-	    }
+	    Light R = it;
+	    Light S1 = get<1>(jt);
+	    Light S2 = get<2>(jt);
 
+	    Vec3f wiR = (R.getPos()-v.p);
+	    Vec3f wiS1 = (S1.getPos()-v.p);
+	    Vec3f wiS2 = (S1.getPos()-v.p);
+
+	    float geometryR = 1 / (wiR.length())*(wiR.length());
+	    float intensityR = R.getIntensity();
+	    float visibilityR = (result[k])[R.getIndex()];
+
+	    float geometryS1 = 1 / (wiS1.length())*(wiS1.length());
+	    float intensityS1 = S1.getIntensity();
+	    float visibilityS1 = (result[k])[S1.getIndex()];
+
+	    float geometryS2 = 1 / (wiS2.length())*(wiS2.length());
+	    float intensityS2 = S2.getIntensity();
+	    float visibilityS2 = (result[k])[S2.getIndex()];
+
+	    if(visibilityS2 == 0){
+	      errorRec+=(geometryS1*intensityS1*visibilityS1-geometryR*intensityR*visibilityR)/(geometryS1*intensityS1*visibilityS1);
+	    } else if (visibilityS1 == 0){
+	      errorRec+=(geometryS2*intensityS2*visibilityS2-geometryR*intensityR*visibilityR)/(geometryS2*intensityS2*visibilityS2);
+	    } else {
+	      errorRec+=(geometryS1*intensityS1*visibilityS1-geometryR*intensityR*visibilityR)/(geometryS1*intensityS1*visibilityS1) + (geometryS2*intensityS2*visibilityS2-geometryR*intensityR*visibilityR)/(geometryS2*intensityS2*visibilityS2);
+	    }
+	  }
+
+	  if (errorRec<errorBound){
+	    root = R;
+	    son1 = S1;
+	    son2= S2;
+	    errorBound = errorRec;
 	  }
 	}
-	}*/
+      }
     }
+
+    for(vector<Light>::iterator it = cut.begin();it!=cut.end();it++){
+      if(root.isEqual(it)){
+	cut.erase(it);
+      }
+    }
+    cut.push_back(son1);
+    cut.push_back(son2);
+    errorTest -=errorBound;
   }
 }
+*/
